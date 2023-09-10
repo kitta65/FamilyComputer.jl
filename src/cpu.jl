14ac68332d69aacd7f30d061714455b2b25026b7
dr666m1 <skndr666m1@gmail.com>
2023-09-10 11:30:43 +0900

    refactored

--- src/cpu.jl
+++ src/cpu.jl
@@ -24,37 +24,38 @@ function run!(cpu::CPU, program::Vector{UInt8})::Nothing
             return
         elseif opcode == 0xa9 # LDA
             param = program[1+cpu.program_conter]
+            lda!(cpu, param)
             cpu.program_conter += 1
-            cpu.register_a = param
-
-            if cpu.register_a == 0
-                cpu.status = cpu.status | 0b0000_0010
-            else
-                cpu.status = cpu.status & 0b1111_1101
-            end
-
-            if cpu.register_a & 0b1000_0000 != 0
-                cpu.status = cpu.status | 0b1000_0000
-            else
-                cpu.status = cpu.status & 0b0111_1111
-            end
         elseif opcode == 0xaa # tax
-            cpu.register_x = cpu.register_a
-
-            if cpu.register_x == 0
-                cpu.status = cpu.status | 0b0000_0010
-            else
-                cpu.status = cpu.status & 0b1111_1101
-            end
-
-            if cpu.register_x & 0b1000_0000 != 0
-                cpu.status = cpu.status | 0b1000_0000
-            else
-                cpu.status = cpu.status & 0b0111_1111
-            end
+            tax!(cpu)
         else
             throw(@sprintf "0x%02x is not implemented" opcode)
         end
+    end
+end
+
+# opcode
+function lda!(cpu::CPU, value::UInt8)
+    cpu.register_a = value
+    update_status_zero_and_negative!(cpu, cpu.register_a)
+end
+
+function tax!(cpu::CPU)
+    cpu.register_x = cpu.register_a
+    update_status_zero_and_negative!(cpu, cpu.register_x)
+end
+
+function update_status_zero_and_negative!(cpu::CPU, result::UInt8)
+    if result == 0
+        cpu.status = cpu.status | 0b0000_0010
+    else
+        cpu.status = cpu.status & 0b1111_1101
+    end
+
+    if cpu.register_a & 0b1000_0000 != 0
+        cpu.status = cpu.status | 0b1000_0000
+    else
+        cpu.status = cpu.status & 0b0111_1111
     end
 end
 
