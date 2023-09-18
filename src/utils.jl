2548a939965790ede9ede8b6c335631d408a33a1
dr666m1 <skndr666m1@gmail.com>
2023-09-18 23:49:21 +0900

    add @flags (only struct definition)

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,14 @@
 function params2addr(lo::UInt8, hi::UInt8)::UInt16
     (UInt16(hi) << 8) + lo
 end
+
+macro flags(T::Symbol, syms...)
+    fields = []
+    for s in syms
+        field = Expr(:(::), s, :Bool)
+        push!(fields, field)
+    end
+
+    block = Expr(:block, fields...)
+    Expr(:struct, true, T, block)
+end
