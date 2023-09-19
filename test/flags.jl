695bd1f49195849451a1bc4dd2f7fc4330f7e189
dr666m1 <skndr666m1@gmail.com>
2023-09-19 23:57:03 +0900

    setter

--- test/flags.jl
+++ test/flags.jl
@@ -5,3 +5,21 @@
     @test typeof(instance.bits) == UInt8
     @test instance.bits == 0b0000_0001
 end
+
+@testset "set" begin
+    FC.@flags FlagName UInt8 a b
+    instance = FlagName(0b0000_0000)
+
+    a!(instance, true)
+    @test instance.bits & 0b01 == 1
+
+    a!(instance, false)
+    @test instance.bits & 0b01 == 0
+
+    b!(instance, true)
+    @test instance.bits & 0b10 == 0b10
+
+    b!(instance, false)
+    @test instance.bits & 0b10 == 0b00
+
+end
