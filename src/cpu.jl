693f3bc3b60c307fdcf8f7e2d69792bc2d50808d
dr666m1 <skndr666m1@gmail.com>
2023-09-29 22:29:36 +0900

    ldx 0xae

--- src/cpu.jl
+++ src/cpu.jl
@@ -141,6 +141,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0xa2 # LDX
         ldx!(cpu, immediate, logger)
         cpu.program_counter += 0x01
+    elseif opcode == 0xae
+        ldx!(cpu, absolute, logger)
+        cpu.program_counter += 0x02
 
     elseif opcode == 0xa0 # LDY
         ldy!(cpu, immediate, logger)
--- test/cpu.jl
+++ test/cpu.jl
@@ -76,7 +76,7 @@ function nestest()
     io = PipeBuffer()
 
     open("../download/nestest.log", "r") do log
-        for _ = 1:820
+        for _ = 1:840
             FC.step!(cpu, io = io)
             actual = readline(io)
             expected = readline(log)[1:73] # TODO test clock cycle
