0e09b8b5f44337b47aebbea486f9034fc02d946b
dr666m1 <skndr666m1@gmail.com>
2023-09-30 15:36:38 +0900

    0x05 ora

--- src/cpu.jl
+++ src/cpu.jl
@@ -179,6 +179,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0x09 # ORA
         ora!(cpu, immediate, logger)
         cpu.program_counter += 0x01
+    elseif opcode == 0x05
+        ora!(cpu, zeropage, logger)
+        cpu.program_counter += 0x01
     elseif opcode == 0x01
         ora!(cpu, indirect_x, logger)
         cpu.program_counter += 0x01
