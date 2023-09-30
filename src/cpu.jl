1ba305bff4a6b8a80b1511afba39946626937077
dr666m1 <skndr666m1@gmail.com>
2023-09-30 21:15:46 +0900

    ldy 0xac

--- src/cpu.jl
+++ src/cpu.jl
@@ -198,6 +198,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0xa4
         ldy!(cpu, zeropage, logger)
         cpu.program_counter += 0x01
+    elseif opcode == 0xac
+        ldy!(cpu, absolute, logger)
+        cpu.program_counter += 0x02
 
     elseif opcode == 0x4a # LSR
         lsr!(cpu, accumulator, logger)
