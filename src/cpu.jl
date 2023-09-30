d536d4d85209327b219c79bb2d159dadac22a80b
dr666m1 <skndr666m1@gmail.com>
2023-09-30 21:21:58 +0900

    and 0x2d

--- src/cpu.jl
+++ src/cpu.jl
@@ -39,6 +39,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0x25
         and!(cpu, zeropage, logger)
         cpu.program_counter += 0x01
+    elseif opcode == 0x2d
+        and!(cpu, absolute, logger)
+        cpu.program_counter += 0x02
     elseif opcode == 0x21
         and!(cpu, indirect_x, logger)
         cpu.program_counter += 0x01
