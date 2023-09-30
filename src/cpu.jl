3589a01cb6ebab9393451aab34a169816ec44a9b
dr666m1 <skndr666m1@gmail.com>
2023-09-30 22:26:36 +0900

    rol 0x2e

--- src/cpu.jl
+++ src/cpu.jl
@@ -268,6 +268,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0x26
         rol!(cpu, zeropage, logger)
         cpu.program_counter += 0x01
+    elseif opcode == 0x2e
+        rol!(cpu, absolute, logger)
+        cpu.program_counter += 0x02
 
     elseif opcode == 0x6a # ROR
         ror!(cpu, accumulator, logger)
--- test/cpu.jl
+++ test/cpu.jl
@@ -76,7 +76,7 @@ function nestest()
     io = PipeBuffer()
 
     open("../download/nestest.log", "r") do log
-        for _ = 1:2920
+        for _ = 1:2960
             FC.step!(cpu, io = io)
             actual = readline(io)
             expected = readline(log)[1:73] # TODO test clock cycle
