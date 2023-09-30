be105c927e3110482bd3aa37e744b11fb0919913
dr666m1 <skndr666m1@gmail.com>
2023-09-30 22:23:06 +0900

    lsr 0x4e

--- src/cpu.jl
+++ src/cpu.jl
@@ -228,6 +228,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0x46
         lsr!(cpu, zeropage, logger)
         cpu.program_counter += 0x01
+    elseif opcode == 0x4e
+        lsr!(cpu, absolute, logger)
+        cpu.program_counter += 0x02
 
     elseif opcode == 0xea # NOP
         nop!(cpu, unspecified, logger)
