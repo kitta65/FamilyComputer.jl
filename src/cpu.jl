67f5db85185d48adbda62a5120bde4c4780784cb
dr666m1 <skndr666m1@gmail.com>
2023-09-30 16:16:23 +0900

    0xe5 sbc

--- src/cpu.jl
+++ src/cpu.jl
@@ -225,6 +225,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0xe9 # SBC
         sbc!(cpu, immediate, logger)
         cpu.program_counter += 0x01
+    elseif opcode == 0xe5
+        sbc!(cpu, zeropage, logger)
+        cpu.program_counter += 0x01
     elseif opcode == 0xe1
         sbc!(cpu, indirect_x, logger)
         cpu.program_counter += 0x01
