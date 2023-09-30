8dbee962de676f32535c65bc4c9da79bdf805a36
dr666m1 <skndr666m1@gmail.com>
2023-09-30 21:22:57 +0900

    eor 0x4d

--- src/cpu.jl
+++ src/cpu.jl
@@ -143,6 +143,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0x45
         eor!(cpu, zeropage, logger)
         cpu.program_counter += 0x01
+    elseif opcode == 0x4d
+        eor!(cpu, absolute, logger)
+        cpu.program_counter += 0x02
     elseif opcode == 0x41
         eor!(cpu, indirect_x, logger)
         cpu.program_counter += 0x01
