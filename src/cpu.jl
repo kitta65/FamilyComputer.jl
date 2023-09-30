7c6ae5378b12286f1300e4053dd28e1743817eb6
dr666m1 <skndr666m1@gmail.com>
2023-09-30 21:19:08 +0900

    bit 0x2c

--- src/cpu.jl
+++ src/cpu.jl
@@ -64,6 +64,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0x24 # BIT
         bit!(cpu, zeropage, logger)
         cpu.program_counter += 0x01
+    elseif opcode == 0x2c
+        bit!(cpu, absolute, logger)
+        cpu.program_counter += 0x02
 
     elseif opcode == 0x30 # BMI
         bmi!(cpu, immediate, logger)
--- test/cpu.jl
+++ test/cpu.jl
@@ -76,7 +76,7 @@ function nestest()
     io = PipeBuffer()
 
     open("../download/nestest.log", "r") do log
-        for _ = 1:2300
+        for _ = 1:2340
             FC.step!(cpu, io = io)
             actual = readline(io)
             expected = readline(log)[1:73] # TODO test clock cycle
