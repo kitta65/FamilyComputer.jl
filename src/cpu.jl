e73cb6ef83290c6c0abd2d72a95aa19967795de7
dr666m1 <skndr666m1@gmail.com>
2023-09-30 21:17:35 +0900

    sty 0x8c

--- src/cpu.jl
+++ src/cpu.jl
@@ -302,6 +302,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0x84 # STY
         sty!(cpu, zeropage, logger)
         cpu.program_counter += 0x01
+    elseif opcode == 0x8c
+        sty!(cpu, absolute, logger)
+        cpu.program_counter += 0x02
 
     elseif opcode == 0xaa # TAX
         tax!(cpu, logger)
