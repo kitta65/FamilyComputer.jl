5d3928d66373f1023f21e0d5b082c0b9626d168f
dr666m1 <skndr666m1@gmail.com>
2023-10-01 17:08:28 +0900

    0x1d ora

--- src/cpu.jl
+++ src/cpu.jl
@@ -315,6 +315,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0x0d
         ora!(cpu, absolute, logger)
         cpu.program_counter += 0x02
+    elseif opcode == 0x1d
+        ora!(cpu, absolute_x, logger)
+        cpu.program_counter += 0x02
     elseif opcode == 0x19
         ora!(cpu, absolute_y, logger)
         cpu.program_counter += 0x02
