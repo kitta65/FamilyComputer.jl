365c244e4924c6777c35089f3b45d75b6820f93e
dr666m1 <skndr666m1@gmail.com>
2023-10-01 17:12:50 +0900

    cmp 0xdd

--- src/cpu.jl
+++ src/cpu.jl
@@ -148,6 +148,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0xcd
         cmp!(cpu, absolute, logger)
         cpu.program_counter += 0x02
+    elseif opcode == 0xdd
+        cmp!(cpu, absolute_x, logger)
+        cpu.program_counter += 0x02
     elseif opcode == 0xd9
         cmp!(cpu, absolute_y, logger)
         cpu.program_counter += 0x02
