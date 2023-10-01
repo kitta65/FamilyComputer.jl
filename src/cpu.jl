2dd775982341f67186b0946c7b02425f110b3c0f
dr666m1 <skndr666m1@gmail.com>
2023-10-01 22:55:40 +0900

    0xaf lax

--- src/cpu.jl
+++ src/cpu.jl
@@ -258,6 +258,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0xa7 # LAX
         lax!(cpu, zeropage, logger, official = false)
         cpu.program_counter += 0x01
+    elseif opcode == 0xaf
+        lax!(cpu, absolute, logger, official = false)
+        cpu.program_counter += 0x02
     elseif opcode == 0xa3
         lax!(cpu, indirect_x, logger, official = false)
         cpu.program_counter += 0x01
