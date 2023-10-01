41012c435965dd20fe6040f8b0601bca640a77ff
dr666m1 <skndr666m1@gmail.com>
2023-10-01 16:37:53 +0900

    adc 0x79

--- src/cpu.jl
+++ src/cpu.jl
@@ -32,6 +32,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0x6d
         adc!(cpu, absolute, logger)
         cpu.program_counter += 0x02
+    elseif opcode == 0x79
+        adc!(cpu, absolute_y, logger)
+        cpu.program_counter += 0x02
     elseif opcode == 0x61
         adc!(cpu, indirect_x, logger)
         cpu.program_counter += 0x01
