364496919379a831237f655b211e3513c502ffe4
dr666m1 <skndr666m1@gmail.com>
2023-10-01 17:22:25 +0900

    0xbe ldx

--- src/cpu.jl
+++ src/cpu.jl
@@ -292,6 +292,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0xae
         ldx!(cpu, absolute, logger)
         cpu.program_counter += 0x02
+    elseif opcode == 0xbe
+        ldx!(cpu, absolute_y, logger)
+        cpu.program_counter += 0x02
 
     elseif opcode == 0xa0 # LDY
         ldy!(cpu, immediate, logger)
