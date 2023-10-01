9a98b9bba67186ff2b093a82bfbf6fe71145c0fb
dr666m1 <skndr666m1@gmail.com>
2023-10-01 23:11:48 +0900

    0x8f sax

--- src/cpu.jl
+++ src/cpu.jl
@@ -463,6 +463,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0x87 # SAX
         sax!(cpu, zeropage, logger, official = false)
         cpu.program_counter += 0x01
+    elseif opcode == 0x8f
+        sax!(cpu, absolute, logger, official = false)
+        cpu.program_counter += 0x02
     elseif opcode == 0x83
         sax!(cpu, indirect_x, logger, official = false)
         cpu.program_counter += 0x01
