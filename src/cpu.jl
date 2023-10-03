21930e1070533625e4b873bf55083f0892af21c8
dr666m1 <skndr666m1@gmail.com>
2023-10-03 23:35:35 +0900

    0xcf dcp

--- src/cpu.jl
+++ src/cpu.jl
@@ -187,6 +187,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0xc7 # DCP
         dcp!(cpu, zeropage, logger, official = false)
         cpu.program_counter += 0x01
+    elseif opcode == 0xcf
+        dcp!(cpu, absolute, logger, official = false)
+        cpu.program_counter += 0x02
     elseif opcode == 0xc3
         dcp!(cpu, indirect_x, logger, official = false)
         cpu.program_counter += 0x01
