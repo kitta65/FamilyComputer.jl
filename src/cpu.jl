00f805ee3786d35b18055cb777f307407d21cb3b
dr666m1 <skndr666m1@gmail.com>
2023-10-04 23:32:59 +0900

    0xfb isc

--- src/cpu.jl
+++ src/cpu.jl
@@ -278,6 +278,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0xef
         isc!(cpu, absolute, logger, official = false)
         cpu.program_counter += 0x02
+    elseif opcode == 0xfb
+        isc!(cpu, absolute_y, logger, official = false)
+        cpu.program_counter += 0x02
     elseif opcode == 0xe3
         isc!(cpu, indirect_x, logger, official = false)
         cpu.program_counter += 0x01
