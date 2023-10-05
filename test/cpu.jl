edd253af9e56e436283b0475c19344934dc887a4
dr666m1 <skndr666m1@gmail.com>
2023-10-05 23:36:45 +0900

    update test/cpu.jl

--- test/cpu.jl
+++ test/cpu.jl
@@ -76,7 +76,7 @@ function nestest()
     io = PipeBuffer()
 
     open("../download/nestest.log", "r") do log
-        for _ = 1:8920
+        for _ = 1:8980 # TODO test until 8991
             FC.step!(cpu, io = io)
             actual = readline(io)
             expected = readline(log)[1:73] # TODO test clock cycle
