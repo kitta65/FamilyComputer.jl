f468f536757222f5b50af15aa6f2c19edb0342f5
dr666m1 <skndr666m1@gmail.com>
2023-10-05 23:33:29 +0900

    0x7f rra

--- src/cpu.jl
+++ src/cpu.jl
@@ -529,6 +529,9 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0x6f
         rra!(cpu, absolute, logger, official = false)
         cpu.program_counter += 0x02
+    elseif opcode == 0x7f
+        rra!(cpu, absolute_x, logger, official = false)
+        cpu.program_counter += 0x02
     elseif opcode == 0x7b
         rra!(cpu, absolute_y, logger, official = false)
         cpu.program_counter += 0x02
