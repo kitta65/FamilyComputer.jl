5ee33cbecd72473639b9e68bdfc7484b24f59ca9
dr666m1 <skndr666m1@gmail.com>
2023-10-05 22:19:06 +0900

    slo 0x07

--- src/cpu.jl
+++ src/cpu.jl
@@ -554,7 +554,10 @@ function step!(cpu::CPU; io::IO = devnull)
     elseif opcode == 0x78 # SEI
         sei!(cpu, unspecified, logger)
 
-    elseif opcode == 0x03 # SLO
+    elseif opcode == 0x07 # SLO
+        slo!(cpu, zeropage, logger, official = false)
+        cpu.program_counter += 0x01
+    elseif opcode == 0x03
         slo!(cpu, indirect_x, logger, official = false)
         cpu.program_counter += 0x01
 
